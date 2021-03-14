7ea0110ad696a701ef785e1207da1c0285703c14
Fdamirez <fdramirez@unicesar.edu.co>
2021-03-14 07:04:27 -0500

    Agregar archivos de proyecto.

---
+++ BuenasPracticayPrincipios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuenasPracticayPrincipios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
