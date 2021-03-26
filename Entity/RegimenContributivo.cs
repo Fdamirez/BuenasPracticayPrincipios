d3a57a5cf31e6eeee5fc93325a1f967f8ffb217e
Fdamirez <fdramirez@unicesar.edu.co>
2021-03-25 21:04:22 -0500

    1

---
+++ Entity/RegimenContributivo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class RegimenContributivo : Regimen 
+    {
+
+        const double SMMLV = 908.526;
+
+        public RegimenContributivo(double numeroLiquidacion, double valorServicio, DateTime fechaLiquidacion,
+            double cuotaModeradora, double tarifa, double topeMax) : base 
+            (numeroLiquidacion,valorServicio,fechaLiquidacion,cuotaModeradora,tarifa,topeMax)
+        {
+        
+        }
+
+        public void CalcularTarifa(double Salario)
+        {
+
+
+            if (Salario < (SMMLV * 2))
+            {
+                Tarifa = 0.15;
+            }
+            else if (Salario < (SMMLV * 5))
+            {
+                Tarifa = 0.20;
+            }
+            else if (Salario >= (SMMLV * 5))
+            {
+                Tarifa = 0.25;
+            }
+
+            //Console .
+
+        }
+
+        public void calculartopeMaximo(double Salario)
+        {
+            if (Salario < (SMMLV * 2))
+            {
+                TopeMax = 250000;
+            }
+            else if (Salario < (SMMLV * 5))
+            {
+                TopeMax = 0.20;
+            }
+            else if (Salario >= (SMMLV * 5))
+            {
+                TopeMax = 0.25;
+            }
+        }
+
+        public void calcularCuotaModeladora()
+        {
+            CuotaModeradora = Tarifa * ValorServicio;
+        }
+
+
+    }
+}
